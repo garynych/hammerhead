ae3059a4561402b8a232b62981170f9eee8d40cb
TomorrowLand_Ace <joaolrpaulo@gmail.com>
2014-04-13 09:15:19 +0100

    fixed build warnings

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -27,6 +27,12 @@
 #include <linux/delay.h>
 #include <linux/input.h>
 #include <linux/jiffies.h>
+<<<<<<< HEAD
+=======
+#include <linux/kernel_stat.h>
+#include <linux/tick.h>
+
+>>>>>>> 93ed5f3... fixed build warnings
 #include <linux/lcd_notify.h>
 
 #define MAKO_HOTPLUG "mako_hotplug"
@@ -42,6 +48,51 @@
 #define NUM_POSSIBLE_CPUS num_possible_cpus()
 
 extern bool boosted;
+
+static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu,
+						  u64 *wall)
+{
+	u64 idle_time;
+	u64 cur_wall_time;
+	u64 busy_time;
+
+	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
+
+	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+
+	idle_time = cur_wall_time - busy_time;
+	if (wall)
+		*wall = jiffies_to_usecs(cur_wall_time);
+
+	return jiffies_to_usecs(idle_time);
+}
+
+static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
+{
+    u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
+
+    if (idle_time == -1ULL)
+	return get_cpu_idle_time_jiffy(cpu, wall);
+    else
+	idle_time += get_cpu_iowait_time_us(cpu, wall);
+
+    return idle_time;
+}
+
+static inline cputime64_t get_cpu_iowait_time(unsigned int cpu, cputime64_t *wall)
+{
+    u64 iowait_time = get_cpu_iowait_time_us(cpu, wall);
+
+    if (iowait_time == -1ULL)
+	return 0;
+
+    return iowait_time;
+}
 
 static struct cpu_stats
 {
@@ -97,6 +148,35 @@ static struct workqueue_struct *wq;
 static struct delayed_work decide_hotplug;
 static struct work_struct suspend, resume;
 
+<<<<<<< HEAD
+=======
+static inline int get_cpu_load(unsigned int cpu)
+{
+	struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+	struct cpufreq_policy policy;
+	u64 cur_wall_time, cur_idle_time;
+	unsigned int idle_time, wall_time;
+	unsigned int cur_load;
+
+	cpufreq_get_policy(&policy, cpu);
+
+	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time);
+
+	wall_time = (unsigned int) (cur_wall_time - pcpu->prev_cpu_wall);
+	pcpu->prev_cpu_wall = cur_wall_time;
+
+	idle_time = (unsigned int) (cur_idle_time - pcpu->prev_cpu_idle);
+	pcpu->prev_cpu_idle = cur_idle_time;
+
+	if (unlikely(!wall_time || wall_time < idle_time))
+		return 0;
+
+	cur_load = 100 * (wall_time - idle_time) / wall_time;
+
+	return (cur_load * policy.cur) / policy.max;
+}
+
+>>>>>>> 93ed5f3... fixed build warnings
 static void cpu_revive(unsigned int cpu)
 {
 	cpu_up(cpu);
